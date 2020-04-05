405f0b2d49f4ae9bbf8c830d4d884231870e93d3
LIMBYEONGCHAE <ithing108@gmail.com>
2020-04-05 12:29:49 +0900

    add stack navigation

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,13 +1,56 @@
+import React from "react";
+import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs" 
 import MoviesScreen from "../screens/Movies";
 import TVScreen from "../screens/TV";
 import SearchScreen from "../screens/Search";
+import { BG_COLOR } from "../constants/Color";
+import TabBarIcon from "../componenets/TabBarIcon";
 
 const TabNavigation = createBottomTabNavigator({
-    Movie: MoviesScreen,
-    TV: TVScreen,
-    Search: SearchScreen
-});
+    Movie: { 
+        screen: MoviesScreen,
+        navigationOptions: {
+            tabBarIcon: ({ focused }) => (
+                <TabBarIcon 
+                    focused={focused} 
+                    name= {Platform.OS === "ios"? "ios-film" : "md-film" }
+                />
+            )
+        }
+    },
+    TV: { 
+        screen: TVScreen,
+        navigationOptions: {
+            tabBarIcon: ({ focused }) => (
+                <TabBarIcon 
+                    focused={focused} 
+                    name= {Platform.OS === "ios"? "ios-tv" : "md-tv" } 
+                />
+            )
+        }
+    },
+    Search: { 
+        screen: SearchScreen,
+        navigationOptions: {
+            tabBarIcon: ({ focused }) => (
+                <TabBarIcon 
+                    focused={focused} 
+                    name= {Platform.OS === "ios"? "ios-search" : "md-search" } 
+                />
+            )
+        }
+    }
+},
+{
+    tabBarOptions:{
+        showLabel: false,
+        style:{
+            backgroundColor: BG_COLOR       
+        }
+    }
+}
+);
 
 export default createAppContainer(TabNavigation); 
--- screens/Movies.js
+++ screens/Movies.js
@@ -1,3 +1,11 @@
 import React from "react";
 import { Text } from "react-native";
-export default () => <Text>Movies</Text>;
+import { TouchableOpacity } from "react-native-gesture-handler";
+export default ({navigation}) => (
+    <>
+    <Text>Movies</Text>;
+    <TouchableOpacity onPress={() => navigation.navigate("Detail")}>
+        <Text>Go to detail</Text>
+    </TouchableOpacity>
+    </>
+);
